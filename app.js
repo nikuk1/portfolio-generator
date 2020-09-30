3a1329605f015c7f0c91e17f63c79ce774dadb0c
vkfbmzef <monkeylordjunior@gmail.com>
2020-09-29 22:58:44 -0700

    fs not defined error present

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+
+
+
+
+// //Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);
+//     }
   
-    console.log('================');
+//     console.log('================');
   
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-  };
+//     //Is the same as this...
+//     profileDataArr.forEach((profileItem) => {
+//       console.log(profileItem)
+//     });
+//   };
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
+
+// hard-wiring the text into the terminal output:
+// const generatePage = () => 'Name: Jane, Github: janehub';
+
+// // interpolated the text using variables passed into the function that created the output
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
